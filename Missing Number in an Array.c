569b064893e5a3fa10390795b1e77953dcc1de77
Aman Kumar Singh <amantiger112007@gmail.com>
2026-04-15 21:55:47 +0530

    Update Missing Number in an Array.c

--- Missing Number in an Array.c
+++ Missing Number in an Array.c
@@ -24,7 +24,7 @@ int main() {
         printf("%d ", arr[i]);
     }
     printf("\n");
-    
+    // Printing the Result.
     int missing = findMissingNumber(arr, n);
     printf("Missing number: %d\n", missing);
     
