39467994e78613acc82c79a9f50c47c53a24da4f
Aman Kumar Singh <amantiger112007@gmail.com>
2026-04-29 21:33:16 +0530

    Refactor Max Subarray Sum implementation

--- Max Subarray Sum using Kadane's Algorithm .c
+++ Max Subarray Sum using Kadane's Algorithm .c
@@ -5,7 +5,7 @@ int main() {
     int n = 10;
     int a[10] = {2, -5, 9, -6, 2, -1, 5, -3, 7,0};
     
-    // Assigning  Variables
+    // Assigning  Variables.
     int max_val = a[0];
     int current_sum = a[0];
 
