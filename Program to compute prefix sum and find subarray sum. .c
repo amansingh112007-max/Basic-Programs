6fb3b18d20ecb2032fc77acaedbe20f40bf561f2
Aman Kumar Singh <amantiger112007@gmail.com>
2026-05-04 22:30:11 +0530

    Refactor prefix sum program for clarity

--- Program to compute prefix sum and find subarray sum. .c
+++ Program to compute prefix sum and find subarray sum. .c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d", &n);
     int arr[n];
     int prefix[n];
-    // Input array elements.
+    // Input array elements
     printf("Enter the elements:\n");
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
